2200d1feb3d0044f7dbcd963233e43071c46290f
Prachi-M-Patil <127610283+Prachi-M-Patil@users.noreply.github.com>
2023-03-27 20:08:45 +0530

    Update index.js

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,88 +1,41 @@
-let userform  = document.getElementById("user-form");
 
-const retrieveEntries = () => {
-    let entries = localStorage.getItem("user-entries");
-    if (entries){
-        entries = JSON.parse(entries);
-    }
-    else{
-        entries = [];
+const fs = require('fs')
 
-    }
-    return entries;
-    }
-    let userEntries = retrieveEntries ();
-    
-    const displayEntries = () => {
-            const entries = retrieveEntries();
-            let table_entries = entries.map((entry) => {
-               const nameCell = `<td class='border px-4 py-2'> ${entry.name}</td>` ;
-               const emailCell = `<td class='border px-4 py-2'> ${entry.email}</td>` ;
-               const passwordCell = `<td class='border px-4 py-2'> ${entry.password}</td>` ;
-               const dobCell = `<td class='border px-4 py-2'> ${entry.dob}</td>` ;
-               const acceptTermsCell = `<td class='border px-4 py-2'>${entry.acceptedTermsAndConditions}</td>`;
+fs.writeFile(
+ 
+  'sample.txt',
 
-               const row = `<tr>${nameCell} ${emailCell} ${passwordCell} ${dobCell} ${acceptTermsCell}</tr>`;
+  'Hello World. Welcome to Node.js File System module.',
 
-               return row;
+  (err) => {
+    if (err) throw err 
 
-            }).join("\n");
+    console.log('File created!') 
+  }
+)
 
-            let table = `<table class="table-auto w-full" style=" border: 1px solid;
-            margin: 0;"><tr>
-            
-            <th class="px-4 py-2">Name</th>
-            <th class="px-4 py-2">Email</th>
-            <th class="px-4 py-2">Password</th>
-            <th class="px-4 py-2">dob</th>
-            <th class="px-4 py-2">accepted terms?</th>
-</tr>${table_entries}</table>`;
+fs.readFile('sample.txt', (err, data) => {
+  if (err) throw err
 
-let details = document.getElementById("table");
-details.innerHTML = table;
+  console.log(data.toString())
+})
 
-}
-const saveUserForm = (event) => {
-    event.preventDefault();
+fs.appendFile('sample.txt', ' This is my updated content', (err) => {
+  if (err) throw err
 
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    const dob = document.getElementById("dob").value;
+  console.log('File updated!')
+})
 
-    const acceptedTermsAndConditions = document.getElementById("acceptTerms").checked;
-    
-    let currentyear = new Date().getFullYear();
-    let birthyear = dob.split("-");
-    let year = birthyear[0];
+fs.rename('sample.txt', 'test.txt', (err) => {
+  if (err) throw err
 
-    let age = currentyear - year;
-    let isValid = (age) => {
-        if (age > 55 || age < 18) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-    if (!isValid(age)) {
-        document.getElementById("dob").style = "border: 2px solid red;"
-        return alert("You must be between age 18 and 55.\nYou are not elligible!");
-    } else {
-        document.getElementById("dob").style = "border: 2px solid #cccc;"
+  console.log('File name updated!')
+})
 
-    const entry ={
-        name,
-        email,
-        password,
-        dob,
-        acceptedTermsAndConditions
-    };
-    userEntries.push(entry);
-
-    localStorage.setItem("user-entries",JSON.stringify(userEntries));
-    displayEntries();
-}
-
+fs.unlink('test.txt', (err) => {
+  if (err) throw err 
+  console.log('File test.txt deleted successfully!') 
+})
 
 
 }
