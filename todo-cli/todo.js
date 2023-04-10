1534c889836364fadaa724e3a58f1843d48ef70a
Prachi-M-Patil <127610283+Prachi-M-Patil@users.noreply.github.com>
2023-04-10 20:44:28 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,45 +1,103 @@
-/* eslint-disable no-undef */
-const todoList = require("../todo");
-const {all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
+const formattedDate = (d) => { 
+   return d.toISOString().split("T")[0]; 
+ }; 
+ const dateToday = new Date(); 
+ const today = formattedDate(dateToday); 
+ const yesterday = formattedDate( 
+   new Date(new Date().setDate(dateToday.getDate() - 1)) 
+ ); 
+ const tomorrow = formattedDate( 
+   new Date(new Date().setDate(dateToday.getDate() + 1)) 
+ );
+const todoList = () => {
+    all = []
+    const add = (todoItem) => {
+      all.push(todoItem)
+    }
+    const markAsComplete = (index) => {
+      all[index].completed = true
+    }
+  
+    const overdue = () => {
+      let overdueCheck = []
+      all.forEach((itm) => {
+        if (itm.dueDate === yesterday && itm.completed === false){
+          overdueCheck.push(itm);
+        }
+      })
+      return overdueCheck.reverse();
+      // Write the date check condition here and return the array
+      // of overdue items accordingly.
+    }
+  
+    const dueToday = () => {
+      let dueTodayCheck = []
+      all.forEach((itm) => {
+        if (itm.dueDate === today) {
+          dueTodayCheck.push(itm);
+        }
+      })
+      return dueTodayCheck.reverse();
+      // Write the date check condition here and return the array
+      // of todo items that are due today accordingly.
+    }
+  
+    const dueLater = () => {
+      let dueLaterCheck = []
+      all.forEach((itm) => {
+        if (itm.dueDate === tomorrow) {
+          dueLaterCheck.push(itm);
+        }
+      })
+      // Write the date check condition here and return the array
+      // of todo items that are due later accordingly.
+      return dueLaterCheck.reverse();
+    }
+  
+    const toDisplayableList = (list) => {
+      let outputarr = [];
+      list.forEach((itm,index) => {
+        if(itm.dueDate === yesterday)
+        {
+          if(itm.completed === true)
+          {
+            outputarr.push("[x]" + " " + itm.title + " " + itm.dueDate);
+          } else {
+            outputarr.push("[ ]" + " " + itm.title + " " + itm.dueDate);
+          }
+        }
+        if (itm.dueDate === today) {
+          delete itm.dueDate;
+          if (itm.completed === true)
+          {
+            outputarr.push("[x]" + " " + itm.title);
+          }
+          else{
+            outputarr.push("[ ]" + " " + itm.title);
+          }
+        }
+        if(itm.dueDate === tomorrow) {
+                outputarr.push("[ ]" + " " + itm.title + " " + itm.dueDate);
+          }
 
-describe("todolist test suite", () => {
-    const formattedDate = (d) => { 
-         return d.toISOString().split("T")[0]; 
-        }; 
-        const dateToday = new Date(); 
-        const today = formattedDate(dateToday); 
-        const yesterday = formattedDate( 
-          new Date(new Date().setDate(dateToday.getDate() - 1)) 
-        ); 
-        const tomorrow = formattedDate( 
-          new Date(new Date().setDate(dateToday.getDate() + 1)) 
-        );
-    
 
-    test("creating new todo", () => {
-        expect(all.length).toBe(0);
-        add({ title: "test new todo.js", dueDate: today, completed: false });
-        expect(all.length).toBe(1);
-    });
-
-    test("mark todo as complete", () => {
-        markAsComplete(0);
-        expect(all[0].completed).toBe(true);
-
-    });
-
-    test("checks retrieval of overdue items", () => {
-        add({ title: "test new todo.js", dueDate: yesterday, completed: false });
-        expect(overdue().length).toBe(0);
-    });
-
-    test("checks retrieval of due today items", () => {
-        expect(dueToday().length).toBe(1);
-    });
-
-    test("checks retrieval of due later items", () => {
-        add({ title: " test new todo.js", dueDate: tomorrow, completed: false});
-        expect(dueLater().length).toBe(0);
-    });
-
-});
+        })
+        outputarr.reverse();
+        let val = outputarr.join("\n");
+        return val;
+      // Format the To-Do list here, and return the output string
+      // as per the format given above.
+    }
+  
+    return {
+      all,
+      add,
+      markAsComplete,
+      overdue,
+      dueToday,
+      dueLater,
+      toDisplayableList
+    };
+  };
+  
+module.exports = todoList;
