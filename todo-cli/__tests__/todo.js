e3940cf476f6edd235a71a765fd28ec49d681271
Prachi-M-Patil <127610283+Prachi-M-Patil@users.noreply.github.com>
2023-04-10 20:46:11 +0530

    Update todo.js

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -3,14 +3,18 @@ const todoList = require("../todo");
 const {all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 
 describe("todolist test suite", () => {
-    let dtToday = new Date();
-    const today = dtToday.toLocaleDateString("en-CA");
-
-    let yesterDay = new Date(new Date().setDate(dueToday.getDate() - 1));
-    yesterDay = yesterDay.toLocaleDateString("en-CA");
-
-    let dueltr = new Date(new Date().setDate(dueToday.getDate() - 1));
-    dueltr = dueltr.toLocaleDateString("en-CA");
+    const formattedDate = (d) => { 
+         return d.toISOString().split("T")[0]; 
+        }; 
+        const dateToday = new Date(); 
+        const today = formattedDate(dateToday); 
+        const yesterday = formattedDate( 
+          new Date(new Date().setDate(dateToday.getDate() - 1)) 
+        ); 
+        const tomorrow = formattedDate( 
+          new Date(new Date().setDate(dateToday.getDate() + 1)) 
+        );
+    
 
     test("creating new todo", () => {
         expect(all.length).toBe(0);
@@ -25,17 +29,17 @@ describe("todolist test suite", () => {
     });
 
     test("checks retrieval of overdue items", () => {
-        add({ title: "test new todo.js", dueDate: yesterDay, completed: false });
-        expect(overdue().length).toBe(1);
+        add({ title: "test new todo.js", dueDate: yesterday, completed: false });
+        expect(overdue().length).toBe(0);
     });
 
     test("checks retrieval of due today items", () => {
-        expect(dueToday().length).toBe(2);
+        expect(dueToday().length).toBe(1);
     });
 
     test("checks retrieval of due later items", () => {
-        add({ title: " test new todo.js", dueDate: dueltr, completed: false});
-        expect(dueLater().length).toBe(1);
+        add({ title: " test new todo.js", dueDate: tomorrow, completed: false});
+        expect(dueLater().length).toBe(0);
     });
 
 });
