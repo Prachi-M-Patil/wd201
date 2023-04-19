d3539d45c9ec7e921226513e4378cfcbc91d0f24
Prachi-M-Patil <127610283+Prachi-M-Patil@users.noreply.github.com>
2023-04-19 20:35:27 +0530

    Create index.js

---
+++ todo-cli/index.js
@@ -0,0 +1,100 @@
+/* eslint-disable no-unused-vars */
+const { connect } = require("./connectDB.js");
+const Todo = require("./TodoModel.js");
+
+const createTodo = async () => {
+  try {
+    await connect();
+    const todo = await Todo.addTask({
+      title: "Second Item",
+      dueDate: new Date(),
+      completed: false,
+    });
+    console.log(`Created todo with ID : ${todo.id}`);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const countItems = async () => {
+  try {
+    const totalCount = await Todo.count();
+    console.log(`Found ${totalCount} items in the tablet!`);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Retrieve the records
+const getAllTodos = async () => {
+  try {
+    const todos = await Todo
+      .findAll
+      //   {
+      //   where: {
+      //     completed: false
+      //   },
+      //   order: [
+      //     ['id', 'DESC']
+      //   ]
+      // }
+      ();
+    const todoList = todos.map((todo) => todo.displayableString()).join("\n");
+    console.log(todoList);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getSingeTodo = async () => {
+  try {
+    const todo = await Todo.findOne({
+      where: {
+        completed: false,
+      },
+      order: [["id", "DESC"]],
+    });
+    console.log(todo.displayableString());
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const updateItem = async (id) => {
+  try {
+    await Todo.update(
+      { completed: true },
+      {
+        where: {
+          id: id,
+        },
+      }
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const deleteItem = async (id) => {
+  try {
+    const deletedRowCount = await Todo.destroy({
+      where: {
+        id: id,
+      },
+    });
+
+    console.log(`Deleted ${deletedRowCount} rows!`);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+(async () => {
+  // await createTodo()
+  // await countItems()
+  // await getSingeTodo()
+  await getAllTodos();
+  // await updateItem(2)
+  await deleteItem(2);
+  await getAllTodos();
+})();
