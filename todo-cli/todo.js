25a9574cc91d0244fb39ec0e086c63e7554f19a8
Prachi-M-Patil <127610283+Prachi-M-Patil@users.noreply.github.com>
2023-04-20 07:03:07 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,9 +1,6 @@
 /* eslint-disable no-undef */
-
-const today = new Date().toISOString().split("T")[0];
-
 const todoList = () => {
-  const all = [];
+  all = [];
   const add = (todoItem) => {
     all.push(todoItem);
   };
@@ -12,37 +9,37 @@ const todoList = () => {
   };
 
   const overdue = () => {
-    return all.filter((item) => item.dueDate < today);
-   
+    // Write the date check condition here and return the array
+    // of overdue items accordingly.
+    x = all.filter((element) => element.dueDate == yesterday);
+    return x;
   };
 
   const dueToday = () => {
-    return all.filter((item) => item.dueDate === today);
-    
+    // Write the date check condition here and return the array
+    // of todo items that are due today accordingly.
+    y = all.filter((element) => element.dueDate == today);
+    return y;
   };
 
   const dueLater = () => {
-    return all.filter((item) => item.dueDate > today);
-
+    // Write the date check condition here and return the array
+    // of todo items that are due later accordingly.
+    z = all.filter((element) => element.dueDate == tomorrow);
+    return z;
   };
 
   const toDisplayableList = (list) => {
-    const output = list
-      .map((item) => {
-        return (
-          (item.completed ? "[x]" : "[ ]") +
-          " " +
-          item.title +
-          (item.dueDate === new Date().toISOString().slice(0, 10)
-            ? ""
-            : " " + item.dueDate)
-        ).trim();
-      })
-      .join("\n");
-    return output;
-
     // Format the To-Do list here, and return the output string
     // as per the format given above.
+    a = list
+      .map((i) => {
+        const isCompelete = i.completed ? "[x]" : "[ ]";
+        const display = i.dueDate == today ? "" : i.dueDate;
+        return `${isCompelete} ${i.title.trim()} ${display.trim()}`;
+      })
+      .join("\n");
+    return a;
   };
 
   return {
@@ -56,8 +53,25 @@ const todoList = () => {
   };
 };
 
-// ####################################### #
-// DO NOT CHANGE ANYTHING BELOW THIS LINE. #
-// ####################################### #
+const todos = todoList();
+
+const formattedDate = (d) => {
+  return d.toISOString().split("T")[0];
+};
+
+var dateToday = new Date();
+const today = formattedDate(dateToday);
+const yesterday = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() - 1))
+);
+const tomorrow = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() + 1))
+);
+
+todos.add({ title: "Submit assignment", dueDate: yesterday, completed: false });
+todos.add({ title: "Pay rent", dueDate: today, completed: true });
+todos.add({ title: "Service Vehicle", dueDate: today, completed: false });
+todos.add({ title: "File taxes", dueDate: tomorrow, completed: false });
+todos.add({ title: "Pay electric bill", dueDate: tomorrow, completed: false });
 
 module.exports = todoList;
