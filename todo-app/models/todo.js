2af17ce7e2fecad838ff110cb9b7b1ea10957353
Prachi-M-Patil <127610283+Prachi-M-Patil@users.noreply.github.com>
2023-05-06 13:05:11 +0530

    Update todo.js

--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -1,14 +1,10 @@
 "use strict";
-const { Model } = require("sequelize");
+const { Model, Op } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+   
     static associate(models) {
-      // define association here
+     
     }
 
     static addTodo({ title, dueDate }) {
@@ -23,8 +19,32 @@ module.exports = (sequelize, DataTypes) => {
       return this.update({ completed: true });
     }
 
-    static getTodos() {
-      return this.findAll();
+    static async getTodos() {
+      return await this.findAll();
+    }
+
+    static async getOverdueTodos() {
+      return await Todo.findAll({
+        where: {
+          dueDate: { [Op.lt]: new Date().toISOString().split("T")[0] },
+        },
+      });
+    }
+
+    static async getDueTodayTodods() {
+      return await Todo.findAll({
+        where: {
+          dueDate: { [Op.eq]: new Date().toISOString().split("T")[0] },
+        },
+      });
+    }
+
+    static async getDueLaterTodos() {
+      return await Todo.findAll({
+        where: {
+          dueDate: { [Op.gt]: new Date().toISOString().split("T")[0] },
+        },
+      });
     }
   }
   Todo.init(
