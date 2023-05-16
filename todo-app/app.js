3f9a00bfc0e0d0cad3a04e38154a814cdab32136
Prachi-M-Patil <127610283+Prachi-M-Patil@users.noreply.github.com>
2023-05-16 17:35:36 +0530

    Update app.js

--- todo-app/app.js
+++ todo-app/app.js
@@ -10,9 +10,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser("shh! some secreat string"));
 app.use(csrf("this_should_be_32_character_long", ["POST", "PUT", "DELETE"]));
 
+// Set EJS as view engine
 
 app.set("view engine", "ejs");
 
+// eslint-disable-next-line no-undef
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", async (request, response) => {
@@ -38,6 +40,7 @@ app.get("/", async (request, response) => {
   }
 });
 
+// eslint-disable-next-line no-unused-vars
 app.get("/todos", async (request, response) => {
   console.log("Todo list");
   try {
@@ -67,7 +70,10 @@ app.post("/todos", async (request, response) => {
 app.put("/todos/:id", async (request, response) => {
   console.log("We have to update a todo with ID:", request.params.id);
   const todo = await Todo.findByPk(request.params.id);
+  //console.log(todo.dataValues.completed)
+  // console.log(request.body)
   try {
+    // const updatedTodo = await todo.markAsCompleted();
     const updatedTodo = await todo.setCompletionStatus(request.body.completed);
     return response.json(updatedTodo);
   } catch (error) {
@@ -75,6 +81,8 @@ app.put("/todos/:id", async (request, response) => {
     return response.status(422).json(error);
   }
 });
+
+// eslint-disable-next-line no-unused-vars
 app.delete("/todos/:id", async (request, response) => {
   console.log("Delete a todo by ID: ", request.params.id);
   try {
@@ -85,4 +93,7 @@ app.delete("/todos/:id", async (request, response) => {
   }
 });
 
+// app.listen(3000, () => {
+//   console.log("started express server at port 3000");
+// });
 module.exports = app;
